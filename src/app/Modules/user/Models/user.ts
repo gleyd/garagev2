a40faa30b7c999530252bd8754d851a39f618a48
jean torres <torres.jean.13730@gmail.com>
2018-01-22 20:28:18 +0100

    modif module user

--- src/app/Modules/user/Models/user.ts
+++ src/app/Modules/user/Models/user.ts
@@ -1,2 +1,12 @@
 export class User {
+
+
+
+
+  constructor(public id: number, public firstName: string, public lastName: string, public phone?: string, public email?: string) {
+
+  }
+
+
+
 }
--- src/app/Modules/user/user.module.ts
+++ src/app/Modules/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserComponent } from './Components/user/user.component';
+import { User } from './Models/user';
+import { AddUserComponent } from './Components/forms/add-user/add-user.component';
+import { ListUserComponent } from './Components/list-user/list-user.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    User
   ],
-  declarations: []
+  declarations: [UserComponent, AddUserComponent, ListUserComponent]
 })
 export class UserModule { }
