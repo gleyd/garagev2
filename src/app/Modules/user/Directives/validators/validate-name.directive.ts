087c595faebde7d4c03866fb5fc3509fce0813fd
jean torres <torres.jean.13730@gmail.com>
2018-01-28 15:45:44 +0100

    validate-name directive ok

--- src/app/Modules/user/Directives/validators/validate-name.directive.ts
+++ src/app/Modules/user/Directives/validators/validate-name.directive.ts
@@ -2,9 +2,13 @@ import { Directive } from '@angular/core';
 import { NG_VALIDATORS, Validator, AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function validatorName(): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any } => {
-
-    return { 'validatorName': { valid: false } };
+ 
+  return (control: AbstractControl): { [key: string]: any } => {
+   
+    var nameRegex = new RegExp("^([a-zA-Z'àâéèêôùûçÀÂÉÈÔÙÛÇ -]{1,75})$");
+    console.log(control.value);
+    const validateName = nameRegex.test(control.value);
+    return validateName ? null :  { 'validatorName': { value: control.value } };
   };
 }
 
