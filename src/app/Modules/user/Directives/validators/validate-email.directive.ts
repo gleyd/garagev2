e3cd2411ffbf72387be1cc18f3057086d4b263a2
jean torres <torres.jean.13730@gmail.com>
2018-01-29 23:42:34 +0100

    module user Formvalidator finish

--- src/app/Modules/user/Directives/validators/validate-email.directive.ts
+++ src/app/Modules/user/Directives/validators/validate-email.directive.ts
@@ -1,10 +1,32 @@
 import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function validatorEmail(): ValidatorFn {
+
+  return (control: AbstractControl): { [key: string]: any } => {
+
+    var emailRegex = new RegExp("^[a-zA-Z0-9_]+([-+.'][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([-.][a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}");
+    console.log(control.value);
+    const validateEmail = emailRegex.test(control.value);
+    return validateEmail ? null : { 'validatorEmail': { value: control.value } };
+  };
+}
 
 @Directive({
-  selector: '[appValidateEmail]'
-})
-export class ValidateEmailDirective {
+  selector: '[ValidateEmail]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: ValidateEmailDirective, multi: true }
 
-  constructor() { }
+  ]
+})
+
+
+export class ValidateEmailDirective implements Validator {
+
+
+  validate(control: AbstractControl): { [key: string]: any; } {
+    return validatorEmail()(control);
+  }
+
 
 }
--- src/app/Modules/user/Directives/validators/validate-phone.directive.ts
+++ src/app/Modules/user/Directives/validators/validate-phone.directive.ts
@@ -1,10 +1,34 @@
 import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function validatorPhone(): ValidatorFn {
+
+  return (control: AbstractControl): { [key: string]: any } => {
+
+    var phoneRegex = new RegExp("^0[0-9]( |-)?([0-9]{2}( |-)?){4}$");
+    console.log(control.value);
+    console.log(phoneRegex);
+    const validatePhone = phoneRegex.test(control.value);
+    console.log(validatePhone);
+    return validatePhone ? null : { 'validatorPhone': { value: control.value } };
+  };
+}
 
 @Directive({
-  selector: '[appValidatePhone]'
-})
-export class ValidatePhoneDirective {
+  selector: '[ValidatePhone]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: ValidatePhoneDirective, multi: true }
 
-  constructor() { }
+  ]
+})
+
+
+export class ValidatePhoneDirective implements Validator {
+
+
+  validate(control: AbstractControl): { [key: string]: any; } {
+    return validatorPhone()(control);
+  }
+
 
 }
